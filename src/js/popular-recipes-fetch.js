3e44ca286511689ff980654a9e2a3158009d14a9
Anastasiia211 <nastik121@ukr.net>
2023-09-19 00:09:46 +0300

    js popular recipes

--- src/js/popular-recipes-fetch.js
+++ src/js/popular-recipes-fetch.js
@@ -1,17 +1,69 @@
 import { fetchPopular } from './API/popular-recipes-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const elements= document.querySelector('.popular-list');
+
+// popularData().then(data => {
+//   // elements.cards.innerHTML = createMarkupPopular(data);
+  elements.addEventListener('click', evt => {
+    const popImgLink = evt.target.closest('.pop-img-link');
+    if (!popImgLink) {
+      return;
+    }
+
+    const clickBtn = popImgLink.querySelector('.button-recipes');
+    onClickBtn();
+    
+
+    function onClickBtn() {
+      const popImg = document.querySelector('.current-vrapper');
+      const backdropPopImg = document.querySelector('.popup');
+
+      popImg.style.display = 'block';
+      backdropPopImg.style.display = 'block';
+   
+    }
+  })
+//  })
+// });
+
 popularData();
 async function popularData() {
   try {
     const result = await fetchPopular();
-    //console.log(result);
-    //elements.cards.innerHTML = createMarkupPopular(result);
+  
+    elements.innerHTML = createMarkupPopular(result);
   } catch {
     Notify.failure('Oops! Something went wrong! Try reloading the page!');
   }
 
+
   function createMarkupPopular(arr) {
-    return arr.map(({ description }) => ``).join('');
+    return arr
+      .map(
+        ({
+          _id,
+          title,
+          description,
+          preview,
+        }) => `<li class="popular-recipes-list">
+        <button value="${_id}" class="pop-img-link">
+        </button>
+      <img
+        id="${_id}"
+        class="img-popular"
+        src="${preview}"
+        alt="${title}"
+      />
+      <div class="popular-card">
+        <h3 class="popular-title">"${title}"</h3>
+        <p class="popular-text">
+          ${description}
+        </p>
+      </div>
+    </li>
+    `
+      )
+      .join('');
   }
 }
