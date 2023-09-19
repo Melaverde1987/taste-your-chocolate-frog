60516a7bf9e816437d0b064f2a865bcdbcff0b65
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-09-20 01:49:52 +0200

    swiper fixed

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,32 +1,22 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import 'swiper/css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const swiper = new Swiper('.swiper-hero', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  slideRole: 'group',
   slidesPerGroup: 1,
-  // slidesPerView: 3,
-
-  // If we need pagination
+  modules: [Pagination],
+  spaceBetween: 20,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     type: 'bullets',
+    dynamicBullets: true,
   },
-
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
 export { swiper };
