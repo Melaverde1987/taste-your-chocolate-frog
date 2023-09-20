090f0fcf2e7ca5d322c172494f87e1982d6ffb71
ZuMa <azuley@gmail.com>
2023-09-21 00:58:40 +0300

    Fix pagenation bullet

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,24 +3,12 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import '../css/partials/hero-swiper.css';
 
 const swiper = new Swiper('.swiper-hero', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   slidesPerGroup: 1,
-  // effect: 'fade',
-  // fadeEffect: {
-  //   crossFade: true,
-  // },
-  // effect: 'flip',
-  // flipEffect: {
-  //   slideShadows: false,
-  // },
-  // virtual: {
-  //   enabled: true,
-  // },
   modules: [Pagination],
   spaceBetween: 20,
   pagination: {
