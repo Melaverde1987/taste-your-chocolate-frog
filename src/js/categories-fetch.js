6cb5eface438c786ca35f4cc05ef17b6b5544834
kruhitka1706 <kruhitka1706@gmail.com>
2023-09-20 10:35:23 +0300

    commit-all-categories

--- src/js/categories-fetch.js
+++ src/js/categories-fetch.js
@@ -1,10 +1,23 @@
 import { fetchCategories } from './API/categories-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+
+const categoryContainer = document.querySelector('.categories');
+const categoriesAll = document.querySelector('.categories-list');
+export const allCategories = document.querySelector('.btn-all-categories');
+const blokCategory = document.querySelector('.categories-block');
+const btnCategory = document.querySelector('.category-btn');
+const categoriesItem = document.querySelector('.categories-element')
+const CATEGORIES_ENDPOINT = '/categories';
+
+
+
 categoriesData();
 async function categoriesData() {
   try {
     const result = await fetchCategories();
+    const categoriesList = createMarkupCategories(result);
+    categoriesAll.insertAdjacentHTML('beforeend', categoriesList);
     //console.log(result);
     //elements.cards.innerHTML = createMarkupCategories(result);
   } catch {
@@ -12,6 +25,11 @@ async function categoriesData() {
   }
 
   function createMarkupCategories(arr) {
-    return arr.map(({ description }) => ``).join('');
+    return arr.map(({ _id, name  }) => 
+    `<li class="categories-element" data-id=${_id}>
+    <button class="category-btn">${name}</button>
+    </li>`
+    ).join('');
   }
 }
+
