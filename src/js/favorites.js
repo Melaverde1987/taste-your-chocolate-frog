f12a91b0fc8a61ec0445141f296a7f413dd646e5
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-09-21 02:31:38 +0300

    fix favorite.js

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,71 +1,73 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { createMarkupGridCard } from './grid-card-fetch';
 
-const btnToFavorite = document.querySelectorAll('.button-favorite-recipes');
+const listRecipe = document.querySelector('.list-recipes');
 
-btnToFavorite.forEach(btn => {
-  btn.addEventListener('click', onClickFavorite);
-});
+listRecipe.addEventListener('click', onClickFavorite);
 
-function onClickFavorite(event) {
-  const buttonFavorite = event.currentTarget;
-  const svgHeart = buttonFavorite.querySelector('.icon-favorite-recipes'); // Находим <svg> иконку
+function onClickFavorite(evt) {
+  if (evt.target.classList.contains('icon-favorite-recipes')) {
+    const name = evt.target.getAttribute('name');
+    const svgHeart = evt.target;
+    svgHeart.classList.toggle('filled-heart');
 
-  const _id = buttonFavorite.getAttribute('name');
+    const storedFavorites =
+      JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
 
-  // Закрашиваем или снимаем заливку для иконки
-  svgHeart.classList.toggle('filled-heart');
+    // Объявляем _id и присваиваем ему значение атрибута 'name'
+    const _id = name;
 
-  // Получаем данные из локального хранилища (если они есть)
-  const storedFavorites =
-    JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    // Проверяем, есть ли уже такая карточка в избранном
+    const existingIndex = storedFavorites.findIndex(
+      recipe => recipe._id === _id
+    );
 
-  // Проверяем, есть ли уже такая карточка в избранном
-  const existingIndex = storedFavorites.findIndex(recipe => recipe._id === _id);
+    if (existingIndex !== -1) {
+      // Если карточка уже в избранном, удаляем её
+      storedFavorites.splice(existingIndex, 1);
+    } else {
+      // Создаем объект для представления карточки в избранном
+      const buttonFavorite = svgHeart
+        .closest('.item-recipes')
+        .querySelector('.button-favorite-recipes');
+      const card = svgHeart.closest('.item-recipes');
+      const titleElement = card.querySelector('.title-recipes');
+      const title = titleElement ? titleElement.textContent : '';
+      const description = card.querySelector(
+        '.description-recipes'
+      ).textContent;
+      const rating = card.querySelector('.rating-recipes').textContent;
+      const thumb = card.querySelector('.img-recipes').src;
 
-  if (existingIndex !== -1) {
-    // Если карточка уже в избранном, удаляем её
-    storedFavorites.splice(existingIndex, 1);
-  } else {
-    // Создаем объект для представления карточки в избранном
-    const card = buttonFavorite.closest('.item-recipes');
-    const titleElement = card.querySelector('.title-recipes');
-    const title = titleElement ? titleElement.textContent : '';
-    const description = card.querySelector('.description-recipes').textContent;
-    const rating = card.querySelector('.rating-recipes').textContent;
-    const thumb = card.querySelector('.img-recipes').src;
+      const favoriteRecipe = {
+        _id,
+        title,
+        description,
+        rating,
+        thumb,
+      };
 
-    const favoriteRecipe = {
-      _id,
-      title,
-      description,
-      rating,
-      thumb,
-    };
+      // Добавляем карточку в избранные
+      storedFavorites.push(favoriteRecipe);
+    }
 
-    // Добавляем карточку в избранные
-    storedFavorites.push(favoriteRecipe);
+    // Сохраняем обновленный массив в локальное хранилище
+    localStorage.setItem('favoriteRecipes', JSON.stringify(storedFavorites));
+    console.log(storedFavorites);
+    Notify.success('Recipe added to favorites!');
   }
-
-  // Сохраняем обновленный массив в локальное хранилище
-  localStorage.setItem('favoriteRecipes', JSON.stringify(storedFavorites));
-  console.log(storedFavorites);
-  Notify.success('Recipe added to favorites!');
 }
 
 renderFavoriteRecipes();
 
 function renderFavoriteRecipes() {
   const favoritesContainer = document.getElementById('favorites-container');
-  // favoritesContainer.innerHTML = ''; // Очищаем контейнер
-
-  // Получаем текущий список карточек из Local Storage
   const storedFavorites =
     JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
 
   // Отрисовываем каждую карточку
   storedFavorites.forEach(recipe => {
-    const cardHTML = createMarkupGridCard(recipe); 
+    const cardHTML = createMarkupGridCard(recipe);
     favoritesContainer.innerHTML += cardHTML;
   });
 }
